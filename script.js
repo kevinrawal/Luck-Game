7affb9ba2c4af1127457e8b225bfc6f0d5eec0a4
KevinRawal <kevinrawal30@gmail.com>
2023-11-09 15:25:09 +0530

    handel edge cases, add refresh game function

--- script.js
+++ script.js
@@ -4,19 +4,35 @@ const input1 = document.getElementById('input-1');
 const input2 = document.getElementById('input-2');
 const btn2 = document.getElementById('btn-2');
 
-let a = 0;
-let b = 0;
+let a = -1;
+let b = -1;
 
 bnt1.addEventListener('click', () => {
     a = input1.value;
+    if(a>10 || a<0){
+        document.getElementById('warning').style.display = 'block';
+        return;
+    }
+    else{
+        document.getElementById('warning').style.display = 'none';
+    }
     input1.hidden = true;
 });
 
 btn2.addEventListener('click', () => {
     b = input2.value;
+    if(b>10 || b<0){
+        document.getElementById('warning').innerHTML = 'The answer must be in range of 1 to 10';
+        document.getElementById('warning').style.display = 'block';
+        return;
+    }
+    else{
+        document.getElementById('warning').innerHTML = 'Please select number between 0 to 10';
+        document.getElementById('warning').style.display = 'none';
+    }
     input2.hidden = true;
-    console.log(a);
-    console.log(b);
+    // console.log(a);
+    // console.log(b);
 });
 
 const finalBtn = document.getElementById('final-btn');
@@ -27,6 +43,15 @@ finalBtn.addEventListener('click',()=>{
 })
 
 function checkWinner(){
+    if(a==-1 || b==-1){
+        document.getElementById('warning').innerHTML = 'Please give the input first';
+        document.getElementById('warning').style.display = 'block';
+        return;
+    }
+    else{
+        document.getElementById('warning').innerHTML = 'Please select number between 0 to 10';
+        document.getElementById('warning').style.display = 'none';
+    }
     const winner = document.getElementById('winner');
     
     if(a==b){
